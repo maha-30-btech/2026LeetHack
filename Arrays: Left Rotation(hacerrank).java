f59b5c4c42b37fb2f7638a69561b14d1fbacb2f9
maha-30-btech <ksmahathysri@gmail.com>
2026-09-11 10:37:50 +0530

    Create Arrays: Left Rotation(hacerrank).java

---
+++ Arrays: Left Rotation(hacerrank).java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+        int n = a.size();
+        d = d % n;
+        List<Integer> rotated = new ArrayList<>();
+        
+        for (int i = d; i < n; i++) {
+            rotated.add(a.get(i));
+        }
+        for (int i = 0; i < d; i++) {
+            rotated.add(a.get(i));
+        }
+        
+        return rotated;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.rotLeft(a, d);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
